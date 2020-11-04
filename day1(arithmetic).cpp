d3fa6ae54200d1fda05d79bbc1814a7ebd2d0b8b
anjana-analyst <65863923+anjana-analyst@users.noreply.github.com>
2020-11-04 09:38:16 +0530

    Create day1(arithmetic).cpp

---
+++ day1(arithmetic).cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int a=10,b=40;
+   //two integers for binary operation
+   //1 + (Basically adds two numbers)
+   cout<<"The addition of two numbers is"<<a+b;
+   //2 -(subtracts two numbers)
+   cout<<"The subtraction of two numbers is"<<a-b;
+   //3 *(multiplies two numbers)
+   cout<<"The multiplication of two numbers is"<<a*b;
+   //4  /(divides two numbers)
+   cout<<"The division of two numbers is"<<a/b;
+   //5 %(gives the remainder of the division)
+   cout<<"The modulo of two numbers is"<<a%b;
+   //6 ++(increments a given digit by 1)
+   cout<<"The increment of a is"<<a++;
+   //7 --(decrements a given digit by 1)
+   cout<<"The decrement of a is"<<a--;
+   return 0;
+  } 
+  //Note: Increment and decrement has classfications which will be covered in later tutorials
