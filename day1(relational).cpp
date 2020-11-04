f73156a769683820dcd46153bbc623e0c5da92d3
anjana-analyst <65863923+anjana-analyst@users.noreply.github.com>
2020-11-04 15:46:24 +0530

    Create day1(relational).cpp

---
+++ day1(relational).cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a=10,b=20;
+//relational operators will give answer in binary either as 1(True)/0(False)
+//operator-1 >-greater
+cout<<(a>b);
+//here it gave 0
+//operator-2 <-lesser
+cout<<(a<b);
+//here it gave 1
+//operator-3 >=-greater than or equal to
+cout<<(a>=b);
+//here it gave 0
+//operator-4 <=-lesser than or equal to
+cout<<(a<=b);
+//here it gave 1
+//operator-5 == -double equal to
+cout<<(a==b);
+//here it gave 0
+//operator-6 !=-not equal to
+cout<<(a!=b);
+//here it gave 1
+return 0;
+}
