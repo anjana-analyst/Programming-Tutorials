d01f611eaca9ad93462cf01e1ecae0d56cb4b77d
anjana-analyst <65863923+anjana-analyst@users.noreply.github.com>
2020-11-15 19:19:29 +0530

    Create operators.js

---
+++ Javascript/basic/operators.js
@@ -0,0 +1,37 @@
+var x=5;
+var y=10;
+//addition
+var add=x+y;
+//subtraction
+var sub=x-y;
+//multiplication
+var mul=x*y;
+//division
+var div=x/y;
+//increment
+x++;
+//decrement
+y--;
+//decimal
+var myDecimal=6.7;
+//assignment operator
+var a=10;
+var b=20;
+a+=10;
+b-=10;
+var c=10;
+c*=10;
+c/=10;
+var myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+var myStr;
+myStr="FirstLine\n\t\\SecondLine\nThirdLine";
+var myStr1;
+myStr1="This is the start." + " "+"This is the end.";
+console.log(myStr1);
+var myStr2,myStr3="";
+myStr2="This is the first sentence.";
+myStr2 += " This is the second sentence.";
+var mystr2_length=myStr2.length;
+myStr3=myStr2[0];
+myStr3=myStr2[myStr2.length-1];
