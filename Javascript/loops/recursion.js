72b4b06432da2e4b0dda79a220bb2f1f2e3e0695
anjana-analyst <65863923+anjana-analyst@users.noreply.github.com>
2020-11-17 12:57:16 +0530

    Update recursion.js

--- Javascript/loops/recursion.js
+++ Javascript/loops/recursion.js
@@ -57,3 +57,30 @@ function lookUpProfile(name, prop)
 }
 
 lookUpProfile("Akira", "likes");
+
+function rangeOfNumbers(startNum, endNum) 
+{
+  if (endNum - startNum === 0) 
+  {
+    return [startNum];
+  } 
+  else {
+    var numbers = rangeOfNumbers(startNum, endNum - 1);
+    numbers.push(endNum);
+    return numbers;
+     }
+};
+
+function countdown(n) 
+{
+  if (n < 1)
+  {
+    return [];
+  } 
+  else 
+  {
+    const arr = countdown(n - 1);
+    arr.unshift(n);
+    return arr;
+  }
+}
