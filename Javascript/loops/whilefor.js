a144104b999a48f06b201725a73816ec6084816a
anjana-analyst <65863923+anjana-analyst@users.noreply.github.com>
2020-11-17 12:37:00 +0530

    Create whilefor.js

---
+++ Javascript/loops/whilefor.js
@@ -0,0 +1,34 @@
+var myArray = [];
+var i=5;
+while(i>=0)
+{
+    myArray.push(i);
+    i--;
+}
+
+console.log(myArray);
+
+var myArray1 = [];
+
+for(var i=1;i<=5;i++)
+{
+    myArray1.push(i);
+}
+console.log(myArray1);
+
+var myArr3 = [ 2, 3, 4, 5, 6];
+
+var total=0;
+for(var i=0;i<myArr3.length;i++)
+{
+    total+=myArr3[i];
+}
+console.log(total);
+
+var myArray4 = [];
+var i = 10;
+do
+{
+  myArray4.push(i);
+  i++;
+}while(i<5);
