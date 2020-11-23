e480ee009c4ece7a3432f355c806dca55ed73f7c
anjana-analyst <65863923+anjana-analyst@users.noreply.github.com>
2020-11-23 12:13:26 +0530

    Create Queue(Class).cpp

---
+++ C++/Queue(Class).cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#define max 100
+
+using namespace std;
+
+class Queue 
+{
+private:
+    int item, i;
+    int queue[max] ;;
+    int rear;
+    int front;
+
+public:
+
+    Queue() 
+    {
+        rear = 0;
+        front = 0;
+    }
+
+    void insert() {
+        if (rear == max)
+            cout << "\nQueue Reached Max!";
+        else {
+            cout << "\nEnter The Value to be Insert : ";
+            cin>>item;
+            queue[rear++] = item;
+        }
+    }
+
+    void removeData() {
+        if (front == rear)
+            cout << "\nQueue is Empty!";
+        else {
+            cout << "Removed Value  :" << queue[front];
+            front++;
+        }
+        
+    }
+    
+    void print()
+    {
+        display();
+    }
+
+ protected:
+    void display() 
+    {
+        for (i = front; i < rear; i++)
+            cout << "\nPosition : " << i << " , Value  : " << queue[i];
+    }
+};
+
+int main() 
+{
+    Queue obj;
+    obj.insert();
+    obj.insert();
+    obj.insert();
+    obj.removeData();
+    obj.print();
+    return 0;
+}
