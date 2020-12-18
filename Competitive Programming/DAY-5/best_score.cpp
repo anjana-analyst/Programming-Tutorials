f012f2299f068c2a4e10eb8106c7bf04dbac0aa8
anjana-analyst <65863923+anjana-analyst@users.noreply.github.com>
2020-12-18 16:16:40 +0530

    Create best_score.cpp

---
+++ Competitive Programming/DAY-5/best_score.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+void best_score(int times[],int n)
+{
+    int temp;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(times[j]>times[j+1])
+            {
+                temp=times[j];
+                times[j]=times[j+1];
+                times[j+1]=temp;
+            }
+        }
+    }
+    cout<<"Best score is "<<times[0];
+}
+
+int main()
+{
+    int arr[7]={66,57,54,53,64,52,59};
+    best_score(arr,7);
+    return 0;
+}
