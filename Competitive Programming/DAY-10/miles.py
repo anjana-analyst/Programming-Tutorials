da25b8dcb3a2dc469446d58aa6db19e0c5bc77ec
anjana-analyst <65863923+anjana-analyst@users.noreply.github.com>
2020-12-23 14:57:52 +0530

    Create miles.py

---
+++ Competitive Programming/DAY-10/miles.py
@@ -0,0 +1,8 @@
+no_of_miles=2052
+no_of_days=6
+stoppings=2
+km=1.609344
+each_day_travel= no_of_miles/no_of_days
+avg_stop= each_day_travel/stoppings;
+avg_stop= avg_stop *km;
+print("Average stop between towns is ",avg_stop)
