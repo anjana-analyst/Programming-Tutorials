25ca8805b654caf6be767d36dab74012522f0b31
anjana-analyst <65863923+anjana-analyst@users.noreply.github.com>
2020-12-31 21:24:32 +0530

    Create max.py

---
+++ Competitive Programming/DAY-18/max.py
@@ -0,0 +1,4 @@
+arr=[12,13,8,10,17]
+arr.sort()
+print("The largest fish length that leffy caught on the day is ",arr[len(arr)-1])
+#print("The largest fish length that leffy caught on the day is ", max(arr))
