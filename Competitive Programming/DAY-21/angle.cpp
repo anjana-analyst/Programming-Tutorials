d1c405ff64787816691cd8e45ada935700bc62b6
anjana-analyst <65863923+anjana-analyst@users.noreply.github.com>
2021-01-03 10:44:05 +0530

    Create angle.cpp

---
+++ Competitive Programming/DAY-21/angle.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int min(int x, int y) 
+{ 
+    return (x < y)? x: y; 
+     
+}
+
+int anglecalc(int hours,int minutes)
+{
+    if(hours<0||minutes<0||hours>12||minutes>60)
+    cout<<"Wrong Input";
+    else if(hours==12)
+    hours=0;
+    else if(minutes==60)
+    {
+        minutes=0;
+        hours+=1;
+        if(hours>12)
+        {
+            hours=hours-12;
+        }
+    }
+    float hour_angle=(hours*60+minutes)*0.5;
+    float minute_angle=6*minutes;
+    
+    float angle=abs(hour_angle-minute_angle);
+    
+    angle=min(360-angle,angle);
+    
+    return angle;
+    
+}
+
+int main()
+{
+    int hours,minutes;
+    cout<<"Enter hours";
+    cin>>hours;
+    cout<<"Enter minutes";
+    cin>>minutes;
+    cout<<anglecalc(hours,minutes);    
+    return 0;
+}
