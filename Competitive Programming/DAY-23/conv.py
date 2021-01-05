21eb2ddf5a74b1a866d9cab5f22eb8d5b922bfba
anjana-analyst <65863923+anjana-analyst@users.noreply.github.com>
2021-01-05 19:31:03 +0530

    Create conv.py

---
+++ Competitive Programming/DAY-23/conv.py
@@ -0,0 +1,9 @@
+def decimalToBinary(num):
+    if num > 1:
+        decimalToBinary(num // 2)
+    print(num % 2, end='')
+
+
+
+number = int(input("Enter any decimal number: "))
+decimalToBinary(number)
