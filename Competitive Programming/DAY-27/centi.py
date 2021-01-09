b189be97e594720672517caeeee524c06a2bb593
anjana-analyst <65863923+anjana-analyst@users.noreply.github.com>
2021-01-09 10:39:21 +0530

    Create centi.py

---
+++ Competitive Programming/DAY-27/centi.py
@@ -0,0 +1,5 @@
+cm=float(input("Enter the input in centimeter"))
+#To convert it to inch,meter and kilometer
+print("The converted value of ",cm,"cm in inch ",cm/2.54)
+print("The converted value of ",cm,"cm in meter ",cm/100)
+print("The converted value of ",cm,"cm in kilometer ",cm/100000)
