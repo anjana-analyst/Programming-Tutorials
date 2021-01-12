28e66d7fc410e41810c73dedc4e9421ec1d33665
anjana-analyst <65863923+anjana-analyst@users.noreply.github.com>
2021-01-12 08:58:00 +0530

    Create add.py

---
+++ Competitive Programming/DAY-30/add.py
@@ -0,0 +1,15 @@
+n1 = int(input("Enter first Number: "))
+n2 = int(input("Enter second Number: "))
+my_sum = n1 + n2
+my_diff = abs(n1 - n2)
+my_prod = n1 * n2
+my_av = (n1+n2) / 2
+min_num = min(n1, n2)
+max_num = max(n1, n2)
+
+print("Sum:",my_sum)
+print("Difference:",my_diff)
+print("Product:",my_prod)
+print("Average:",my_av)
+print("Minimum:",min_num)
+print("Maximum:",max_num)
